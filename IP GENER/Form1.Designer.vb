5fb2085981847ba06006ecdad330e51444ca5d6a
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-08-08 11:16:44 +0300

    Add files via upload

---
+++ IP GENER/Form1.Designer.vb
@@ -0,0 +1,378 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmbBase = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbLocation = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown7 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown8 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(274, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(277, 39)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IP GENERATOR"
+        '
+        'cmbBase
+        '
+        Me.cmbBase.FormattingEnabled = True
+        Me.cmbBase.Items.AddRange(New Object() {"MUH", "SAK"})
+        Me.cmbBase.Location = New System.Drawing.Point(181, 119)
+        Me.cmbBase.Name = "cmbBase"
+        Me.cmbBase.Size = New System.Drawing.Size(224, 24)
+        Me.cmbBase.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(73, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Base"
+        '
+        'cmbLocation
+        '
+        Me.cmbLocation.FormattingEnabled = True
+        Me.cmbLocation.Items.AddRange(New Object() {"Hangar 1", "Hangar 2", "Hangar 3", "Hangar 4"})
+        Me.cmbLocation.Location = New System.Drawing.Point(181, 172)
+        Me.cmbLocation.Name = "cmbLocation"
+        Me.cmbLocation.Size = New System.Drawing.Size(224, 24)
+        Me.cmbLocation.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(73, 175)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Location"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"IT BLOCK", "ADMIN BLOCK", "ACCOUNTS BLOCK", "TECH BLOCK", "CS BLOCK"})
+        Me.ComboBox3.Location = New System.Drawing.Point(510, 119)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(198, 24)
+        Me.ComboBox3.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(438, 122)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Block"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(438, 175)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 17)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Status"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"Active", "Inactive", "Planned"})
+        Me.ComboBox4.Location = New System.Drawing.Point(510, 172)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(198, 24)
+        Me.ComboBox4.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(206, 230)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(406, 22)
+        Me.TextBox1.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(156, 233)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 17)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "VLAN"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(181, 308)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown1.TabIndex = 11
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {198, 0, 0, 0})
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(262, 308)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown2.TabIndex = 12
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {198, 0, 0, 0})
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(342, 308)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown3.TabIndex = 13
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(421, 308)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown4.TabIndex = 14
+        Me.NumericUpDown4.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(73, 310)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 17)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "IP RANGE 1"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(73, 370)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(85, 17)
+        Me.Label14.TabIndex = 23
+        Me.Label14.Text = "IP RANGE 2"
+        '
+        'NumericUpDown5
+        '
+        Me.NumericUpDown5.Location = New System.Drawing.Point(421, 368)
+        Me.NumericUpDown5.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown5.Name = "NumericUpDown5"
+        Me.NumericUpDown5.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown5.TabIndex = 22
+        Me.NumericUpDown5.Value = New Decimal(New Integer() {255, 0, 0, 0})
+        '
+        'NumericUpDown6
+        '
+        Me.NumericUpDown6.Location = New System.Drawing.Point(342, 368)
+        Me.NumericUpDown6.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown6.Name = "NumericUpDown6"
+        Me.NumericUpDown6.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown6.TabIndex = 21
+        Me.NumericUpDown6.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'NumericUpDown7
+        '
+        Me.NumericUpDown7.Location = New System.Drawing.Point(262, 368)
+        Me.NumericUpDown7.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown7.Name = "NumericUpDown7"
+        Me.NumericUpDown7.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown7.TabIndex = 20
+        Me.NumericUpDown7.Value = New Decimal(New Integer() {198, 0, 0, 0})
+        '
+        'NumericUpDown8
+        '
+        Me.NumericUpDown8.Location = New System.Drawing.Point(181, 368)
+        Me.NumericUpDown8.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown8.Name = "NumericUpDown8"
+        Me.NumericUpDown8.Size = New System.Drawing.Size(63, 22)
+        Me.NumericUpDown8.TabIndex = 19
+        Me.NumericUpDown8.Value = New Decimal(New Integer() {198, 0, 0, 0})
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(577, 298)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(131, 153)
+        Me.TextBox2.TabIndex = 24
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(507, 301)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(64, 17)
+        Me.Label8.TabIndex = 25
+        Me.Label8.Text = "Remarks"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(181, 417)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(279, 34)
+        Me.TextBox3.TabIndex = 26
+        Me.TextBox3.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(100, 427)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(58, 17)
+        Me.Label9.TabIndex = 27
+        Me.Label9.Text = "Counter"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(483, 481)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 50)
+        Me.Button1.TabIndex = 28
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(618, 481)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 50)
+        Me.Button2.TabIndex = 29
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(810, 571)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.NumericUpDown5)
+        Me.Controls.Add(Me.NumericUpDown6)
+        Me.Controls.Add(Me.NumericUpDown7)
+        Me.Controls.Add(Me.NumericUpDown8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.cmbLocation)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmbBase)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cmbBase As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbLocation As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NumericUpDown3 As NumericUpDown
+    Friend WithEvents NumericUpDown4 As NumericUpDown
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents NumericUpDown5 As NumericUpDown
+    Friend WithEvents NumericUpDown6 As NumericUpDown
+    Friend WithEvents NumericUpDown7 As NumericUpDown
+    Friend WithEvents NumericUpDown8 As NumericUpDown
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ IP GENER/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    ' u will understand later
+    Dim a As String = "."
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' pass on the start ip and end ip to a counter function upon the page load
+
+        '
+        'ReadVal()
+    End Sub
+    'main function ro recognize the values change and update it accordingly
+    Private Sub ReadVal() 
+        'take in the box values for IP Range 1
+        Dim a As String = NumericUpDown4.Value.ToString()
+        Dim b As String = NumericUpDown3.Value.ToString()
+        Dim c As String = NumericUpDown2.Value.ToString()
+        Dim d As String = NumericUpDown1.Value.ToString()
+        'Console.WriteLine(a)
+        Dim IP1 As String = d + "." + c + "." + b + "." + a
+        'Console.WriteLine(IP1)
+        'take in the box values for IP Range 2
+        Dim a1 As String = NumericUpDown5.Value.ToString()
+        Dim b1 As String = NumericUpDown6.Value.ToString()
+        Dim c1 As String = NumericUpDown7.Value.ToString()
+        Dim d1 As String = NumericUpDown8.Value.ToString()
+        'Console.WriteLine(a)
+        Dim IP2 As String = d1 + "." + c1 + "." + b1 + "." + a1
+        'Console.WriteLine(IP2)
+        Counter(IP1, IP2)
+    End Sub
+
+    ' display the difference into the counter before adding the subnet
+    Private Sub NumericUpDown4_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown4.ValueChanged
+        'take in the box values for IP Range 1
+
+        ReadVal()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Dispose()
+    End Sub
+
+    'Add On Later
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Update into Database
+        Console.WriteLine("CLICKED THE SAVE BUTTON")
+    End Sub
+
+    Private Sub LoadDifference(a As Double)
+        TextBox3.Text = a
+        Button1.Enabled = True
+    End Sub
+    Private Sub LoadDifference2(a As Double)
+        'If Form1
+        'TextBox3.Text = ""
+        'Console.WriteLine(TextBox3.Text)
+        'Dim b As Double = CDbl(TextBox3.Text)
+        'Console.WriteLine(b)
+        'a = a * -1
+        'If a < 0 Then
+        If a < 0 Then
+            TextBox3.Text = a
+            Button1.Enabled = False
+            Dim btn = Button1()
+            btn.BackColor = SystemColors.GradientActiveCaption
+        End If
+    End Sub
+
+    ' found the count and now after that display it on the counter
+    ' Create a function with 2 params as Strings that is the conjoined value of 4 boxes
+    Private Sub Counter(IP1 As String, IP2 As String)
+        Dim startiprange As Net.IPAddress = Net.IPAddress.Parse(IP1)
+        Dim endiprange As Net.IPAddress = Net.IPAddress.Parse(IP2)
+
+        'reverse address bytes for conversion to integer
+        Dim strtip() As Byte = startiprange.GetAddressBytes
+        Array.Reverse(strtip)
+        Dim endip() As Byte = endiprange.GetAddressBytes
+        Array.Reverse(endip)
+
+        'convert
+        Dim ips As UInt32 = BitConverter.ToUInt32(strtip, 0)
+        Dim ipe As UInt32 = BitConverter.ToUInt32(endip, 0)
+        Dim c As Double = 0
+        Dim k1 As Double = Convert.ToDouble(ips)
+        Dim k2 As Double = Convert.ToDouble(ipe)
+        'Dim Diff As UInt32 = ips - ipe
+        Console.WriteLine(ips)
+        Console.WriteLine(ipe)
+        If ips > ipe Then
+            Console.WriteLine("Smaller")
+            Dim c1 As Double = Convert.ToDouble(ips)
+            Dim c2 As Double = Convert.ToDouble(ipe)
+            Console.WriteLine(c2 - c1)
+            LoadDifference2(c2 - c1)
+        ElseIf k1 = k2 Then
+            LoadDifference(0)
+        Else
+            For anip As UInt32 = ips To ipe
+                Dim ipbyt() As Byte = BitConverter.GetBytes(anip)
+                'reverse and create ip address
+                Array.Reverse(ipbyt)
+                Dim ipaddr As New Net.IPAddress(ipbyt)
+                'Debug.WriteLine(ipaddr.ToString)
+                c = c + 1
+            Next
+            'Console.WriteLine(c)
+            'c = c
+            LoadDifference(c)
+        End If
+
+    End Sub
+    Private Sub NumericUpDown5_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown5.ValueChanged
+        ReadVal()
+    End Sub
+End Class
